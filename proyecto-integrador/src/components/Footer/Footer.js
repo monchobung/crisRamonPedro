8a14e624683edce3cf845de8281b489d99be9645
PedroMangoldt <prodriguezmangoldt@udesa.edu.ar>
2025-09-12 15:06:20 -0300

    Agregue las carpetas de components, data y screens. Empeze con el header y el footer

---
+++ proyecto-integrador/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css'
+
+function Footer(){
+    return(
+        <footer>
+            <p>Pedro Rodriguez Mangoldt - Ramon Bunge - Cristobal Camdessus</p>
+        </footer>
+    )
+}
+
+export default Footer;
