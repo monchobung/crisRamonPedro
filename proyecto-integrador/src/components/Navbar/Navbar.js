511c2089219420ad7b8a5dc16df549bfc4d70757
monchobung <bunger@udesa.edu.ar>
2025-09-17 11:36:24 -0300

    header

--- proyecto-integrador/src/components/Navbar/Navbar.js
+++ proyecto-integrador/src/components/Navbar/Navbar.js
@@ -4,12 +4,15 @@ import './navbar.css';
 function Navbar(){
   return (
     <nav>
+      <img src="/cinetic.png" alt="Logo" className="logo"/>
+
       <ul className="nav nav-tabs my-4">
         <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
-        <li className="nav-item"><a className="nav-link" href="/peliculaspopulares">Peliculas populares</a></li>
-        <li className="nav-item"><a className="nav-link" href="/peliculascartelera">Peliculas en cartelera</a></li>
+        <li className="nav-item"><a className="nav-link" href="/peliculaspopulares">Películas populares</a></li>
+        <li className="nav-item"><a className="nav-link" href="/peliculascartelera">Películas en cartelera</a></li>
         <li className="nav-item"><a className="nav-link" href="/favoritos">Favoritas</a></li>
       </ul>
+
       <form className="search-form" action="results.html" method="get">
         <input type="text" name="searchData" placeholder="Buscar..." />
         <button type="submit" className="btn btn-success btn-sm">Buscar</button>
