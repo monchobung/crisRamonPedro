dadb2a2bbd453c655a6245877a4eb9266c4aae57
monchobung <bunger@udesa.edu.ar>
2025-09-17 11:49:15 -0300

    header css arreglado

--- proyecto-integrador/src/components/Navbar/Navbar.js
+++ proyecto-integrador/src/components/Navbar/Navbar.js
@@ -4,19 +4,27 @@ import './navbar.css';
 function Navbar(){
   return (
     <nav>
-      <img src="/cinetic.png" alt="Logo" className="logo"/>
+      {/* Logo */}
+      <img 
+        src="/cinetic.png" 
+        alt="Logo UdeSA Movies" 
+        className="logo"
+      />
 
-      <ul className="nav nav-tabs my-4">
-        <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
-        <li className="nav-item"><a className="nav-link" href="/peliculaspopulares">Películas populares</a></li>
-        <li className="nav-item"><a className="nav-link" href="/peliculascartelera">Películas en cartelera</a></li>
-        <li className="nav-item"><a className="nav-link" href="/favoritos">Favoritas</a></li>
-      </ul>
+      {/* Contenedor de links + buscador */}
+      <div className="nav">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/peliculaspopulares">Películas populares</a></li>
+          <li><a href="/peliculascartelera">Películas en cartelera</a></li>
+          <li><a href="/favoritos">Favoritas</a></li>
+        </ul>
 
-      <form className="search-form" action="results.html" method="get">
-        <input type="text" name="searchData" placeholder="Buscar..." />
-        <button type="submit" className="btn btn-success btn-sm">Buscar</button>
-      </form>
+        <form className="search-form" action="results.html" method="get">
+          <input type="text" name="searchData" placeholder="Buscar..." />
+          <button type="submit" className="btn-sm">Buscar</button>
+        </form>
+      </div>
     </nav>
   );
 }
