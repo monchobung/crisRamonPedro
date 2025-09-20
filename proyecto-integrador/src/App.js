ecfcf5646cfff7fcdbb48e55e074228f3dc4c885
ccamdessus <ccamdessus@udesa.edu.ar>
2025-09-20 17:08:58 -0300

    Saque 2 carpetas innecesarias

--- proyecto-integrador/src/App.js
+++ proyecto-integrador/src/App.js
@@ -3,8 +3,6 @@ import Home from "./screens/Home/Home";
 import Detalle from "./screens/Detalle/Detalle";
 import DetalleS from "./screens/DetalleS/DetalleS";
 import Favoritos from "./screens/Favoritos/Favoritos";
-import Series from "./screens/Series/Series";
-import Peliculas from "./screens/Peliculas/Peliculas";
 import PeliculasPopulares from "./screens/PeliculasPopulares/PeliculasPopulares";
 import PeliculasCartelera from "./screens/PeliculasCartelera/PeliculasCartelera";
 import SeriesPopulares from "./screens/SeriesPopulares/SeriesPopulares";
@@ -23,8 +21,6 @@ function App() {
         <Route path="/detalle/pelicula/:id" component={Detalle} />
         <Route path="/detalle/series/:id" component={DetalleS} />
         <Route path="/favoritos" component={Favoritos} />
-        <Route path="/peliculas" component={Peliculas} />
-        <Route path="/series" component={Series} />
         <Route path="/peliculasPopulares" component={PeliculasPopulares} />
         <Route path="/peliculasCartelera" component={PeliculasCartelera} />
         <Route path="/seriesPopulares" component={SeriesPopulares} />
