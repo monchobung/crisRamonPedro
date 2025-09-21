97dc72240ea033255230e06bd0ff041a563cbf6e
ccamdessus <ccamdessus@udesa.edu.ar>
2025-09-21 14:19:31 -0300

    Boton de ver mas y menos (detalle) en las series

--- proyecto-integrador/src/components/SerieCard/SerieCard.js
+++ proyecto-integrador/src/components/SerieCard/SerieCard.js
@@ -1,22 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Component } from 'react';
 
-function SerieCard(props){
-  const data = props.data;
+class PeliculaCard extends Component {
+  constructor (props){
+    super(props)
+    this.state={
+      verMas: false,
+      botonVerMas: "Ver mas"
+    }
+  }
+  
+  botonVerMas(){
+    if (this.state.verMas === false){
+      this.setState({
+        verMas:true,
+        botonVerMas:"Ver menos"
+      })
+    }
+    else{
+      this.setState({
+        verMas:false,
+        botonVerMas:"Ver mas"
+      })
 
+    }
+  }
+
+    render(){
+  
   return (
-    <article className="single-card-tv">
+    <article className="single-card-movie">
       <div className="cardBody">
-      <img className="col-md-6" src={`https://image.tmdb.org/t/p/w500${data.poster_path}`} alt="Imagen"/>
-        <h5 className="card-title">{data.original_name}</h5>
-        <p className="card-text">{data.overview}</p>
-        <Link className="btn btn-primary" to={`/detalle/series/${data.id}`}>
+      <img className="col-md-6" src={`https://image.tmdb.org/t/p/w500${this.props.data.poster_path}`} alt="Imagen"/>
+        <h5 className="card-title">{this.props.data.title}</h5>
+        <div className="descripcion">
+        {this.state.verMas===true ? <div><p className="card-text">{this.props.data.overview}</p></div>: ""}
+        <button onClick={()=> this.botonVerMas()}>{this.state.botonVerMas}</button> 
+        </div>
+        <Link className="btn btn-primary" to={`/detalle/pelicula/${this.props.data.id}`}>
           Ir a detalle
-        </Link>
-        
+        </Link> 
       </div>
     </article>
   );
-}
+    }}
 
-export default SerieCard;
+export default PeliculaCard;
